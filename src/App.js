fa7f89bc587baab999b6eb5f6c1d8057fa425a56
Nicocere <nm.cerecedo@outlook.com>
2022-08-19 12:58:35 -0300

    nuevo comienzo

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import Header from './componentes/Header/Header';
+import Main from './componentes/Main/Main';
+import Footer from './componentes/Footer/Footer';
+
+
+
+function App() {
+  return (
+    <>
+    <Header/>
+    <Main/>
+    <Footer/>
+
+    </>
+
+  );
+}
+
+export default App;
